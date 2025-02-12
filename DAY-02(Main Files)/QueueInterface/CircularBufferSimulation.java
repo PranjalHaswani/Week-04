3735f9faa358e8f98bd1670827d9591468017654
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-02-12 12:56:01 +0530

    [Pranjal_Haswani]add. Circular Buffer Simulation File.

---
+++ DAY-02(Main Files)/QueueInterface/CircularBufferSimulation.java
@@ -0,0 +1,49 @@
+package com.day2.collections.QueueInterface;
+
+import java.util.Arrays;
+
+public class CircularBufferSimulation<T> {
+    private Object[] buffer;
+    private int head;
+    private int tail;
+    private int size;
+    private int capacity;
+
+    public CircularBufferSimulation(int capacity) {
+        this.capacity = capacity;
+        this.buffer = new Object[capacity];
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+    }
+
+    public void insert(T value) {
+        buffer[tail] = value;
+        tail = (tail + 1) % capacity;
+        if (size < capacity) {
+            size++;
+        } else {
+            head = (head + 1) % capacity; // Overwrite oldest element
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public T[] getBufferContents() {
+        Object[] contents = new Object[size];
+        for (int i = 0; i < size; i++) {
+            contents[i] = buffer[(head + i) % capacity];
+        }
+        return (T[]) contents;
+    }
+
+    public static void main(String[] args) {
+        CircularBufferSimulation<Integer> buffer = new CircularBufferSimulation<>(3);
+        buffer.insert(1);
+        buffer.insert(2);
+        buffer.insert(3);
+        System.out.println("Buffer: " + Arrays.toString(buffer.getBufferContents())); // Output: [1, 2, 3]
+
+        buffer.insert(4);
+        System.out.println("Buffer: " + Arrays.toString(buffer.getBufferContents())); // Output: [2, 3, 4]
+    }
+}
